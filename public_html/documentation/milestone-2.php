b01f0de76eb72b68c2d52dabef1e188cd2f67b5a
Jeffrey Cooper <jcooper37@cnm.edu>
2017-05-12 09:42:38 -0600

    Add Milestone 2 documentation file

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--@author: Jeffrey Cooper <jcooper37@cnm.edu>
+	Date: 12-May-17 Time: 8:27 AM-->
+	<head>
+		<meta charset="utf-8" />
+		<title>PWP for Jeffrey Cooper - Milestone 2</title>
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="styles-documentation.css" type="text/css" />
+	</head>
+	<body>
+		<header>
+			<h1>PWP for Jeffrey Cooper</h1>
+			<h2>Milestone 2</h2>
+		</header>
+		<main>
+			<h3>Content Strategy</h3>
+			<!--1. Document the kind of content that you plan to feature on your site, and the purpose and strategy behind it. You are not expected to have any actual content prepared at this time, rather only an idea of the kind of content you would like to feature.
+
+Keep this section brief and succinct. Explain your content plan in detail in one paragraph or less per content section.-->
+
+
+			<h3>Wireframes</h3>
+			<!--One mobile and one desktop wireframe will be required for your project.
+
+Your wireframes may be created using the tool of your choice, but they must be in an electronic format.
+
+Format choices for your wireframes may include, but are not limited to, the following:
+	1. Online wireframing tools such as draw.io, balsamiq, wireframe.cc, Google Drawings (via Google Drive), etc.
+	2. Photoshop/Illustrator/GIMP/MacPaint/Inkscape, etc.
+
+Place your wireframe images in your milestone-2.php document. Image files must be less than 2MB in size, and in SVG, JPEG, PNG, or GIF format.
+-->
+
+		</main>
+	</body>
+</html>

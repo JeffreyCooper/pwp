d5163e825308acfaf89869b5b7669dfc642ae19f
Jeffrey Cooper <jcooper37@cnm.edu>
2017-05-14 20:53:36 -0600

    Milestone 2a Content Strategy

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -18,8 +18,15 @@
 			<!--1. Document the kind of content that you plan to feature on your site, and the purpose and strategy behind it. You are not expected to have any actual content prepared at this time, rather only an idea of the kind of content you would like to feature.
 
 Keep this section brief and succinct. Explain your content plan in detail in one paragraph or less per content section.-->
-
-
+			<p>The Content Strategy of this website is listed below and divided into the five relevant sections of the website.</p>
+			<ol>
+				<li>The initial section will contain content that is visually engaging and feature rich, though editorial content will be minimal. The focus of this section is to introduce the subject by name and industry and to effect branding on the visitor.</li>
+				<li>The second section will contain content that promotes the subject's end product. Verbiage will again be light, only summarizing various kinds of website development that either has been created in the past or can be created in the future. The focus of this section is on appealing to potential clients for freelance work. Branding is a key component of this section.</li>
+				<li>The third section will contain biographical information about the subject's background, education, experience and interests. Verbiage is more important here, with very specific references to skills, degrees, crafts, networks and interests. Although visuals are less important here, they should be more plentiful (thumbnails or galleries on pass-through) and more specific to enhance the verbiage as a footnote would do in a research paper. Branding is minimalist in this section.</li>
+				<li>The fourth section will be the most loose and creative in style. It will refer to interests and hobbies outside career paths and industries. This section personalizes the subject to develop identifiable psychological relevancy for visitors to connect with the subject. This is the emotive section and should have creative images, styles and verbiage. The focus of this section is to "seal the deal" with any visitor who might be considering engaging the subject. Branding in this section must be very creative and either subtle or humorous.</li>
+				<li>The fifth and final section is for contacting the subject. This section will be minimalistic so as not to intimidate the visitor. A well chosen, solitary image that makes the visitor grin is included. A quote, chosen by the subject, stands alone on the page. The focus of this section is to be as inviting to the visitor as possible as well as to provide fodder for discussion in any initial contact between the parties. Branding on this section should be subliminal. After submitting the online form, the visitor will be directed to an animated gif or other visually engaging feature that thanks the visitor and also imprints the overall branding of the site.</li>
+			</ol>
+		</br>
 			<h3>Wireframes</h3>
 			<!--One mobile and one desktop wireframe will be required for your project.
 
@@ -31,7 +38,9 @@ Format choices for your wireframes may include, but are not limited to, the foll
 
 Place your wireframe images in your milestone-2.php document. Image files must be less than 2MB in size, and in SVG, JPEG, PNG, or GIF format.
 -->
+			<h4>Mobile Wireframe</h4>
 
+			<h4>Desktop Wireframe</h4>
 		</main>
 	</body>
 </html>

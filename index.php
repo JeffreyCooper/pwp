8a7d2c7e36a1e8afead427c8c667efe5e2a63fa8
Jeffrey Cooper <jcooper37@cnm.edu>
2017-05-30 11:04:29 -0600

    Add index.php code

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--
+	PWP for JCIT.INFO
+	@author Jeffrey Cooper <jcooper37@cnm.edu>
+	-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Custome CSS -->
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+		<!-- Add FontAwesome -->
+		<script src="https://use.fontawesome.com/1a5ce32ebc.js"></script>
+
+<!--		TODO: Add family for Google Fonts-->
+<!--		Add Google Fonts-->
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=???">
+
+		<!-- jQuery 3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- jQuery 3.0 map -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.map" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Jeffrey Cooper</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">BootStrap</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="http://crooksandliars.com/latest" target="_blank">Crooks and Liars</a></li>
+								<li><a href="https://www.democraticunderground.com/" target="_blank">Democratic Underground</a></li>
+								<li><a href="http://talkingpointsmemo.com/" target="_blank">TPM</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+			<main>
+
+				<section>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="jumbotron text-center">
+									<h1>Bootstrap Challenge</h1>
+									<p>It's ten thousand miles between Kings landing and the wall. A Lannister always pays his debts.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section class="bg-info">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3">
+								<img class="image-responsive image-container" src="http://placekitten.com.s3.amazonaws.com/homepage-samples/200/140.jpg" alt="Kitteh">
+							</div>
+							<div class="col-md-9">
+								<p>You know nothing, Jon Snow. The bear and the maiden fair. Bastards are born of passion, aren't they? We don't despise them in Dorne. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. It is rare to meet a Lannister who shares my enthusiasm for dead Lannisters. Unbowed, Unbent, Unbroken. Unbowed, Unbent, Unbroken. Bastards are born of passion, aren't they? We don't despise them in Dorne. The night is dark and full of terrors. A good act does not wash out the bad, nor a bad act the good. Each should have its own reward. More pigeon pie, please. More pigeon pie, please. The rains of castamere. It's ten thousand miles between Kings landing and the wall. When you play the game of thrones, you win or you die. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. All men must die. Winter is coming. It is rare to meet a Lannister who shares my enthusiasm for dead Lannisters.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3 text-center">
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+								<div class="panel panel-default text-left">
+									<div class="panel-body">
+										The night is dark and full of terrors. Bastards are born of passion, aren't they? We don't despise them in Dorne. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. The War of the 5 kings.
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3 text-center">
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+								<div class="panel panel-default text-left">
+									<div class="panel-body">
+										The night is dark and full of terrors. Bastards are born of passion, aren't they? We don't despise them in Dorne. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. The War of the 5 kings.
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3 text-center">
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+								<div class="panel panel-default text-left">
+									<div class="panel-body">
+										The night is dark and full of terrors. Bastards are born of passion, aren't they? We don't despise them in Dorne. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. The War of the 5 kings.
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3 text-center">
+							<span class="fa-stack fa-5x">
+								<i class="fa fa-circle fa-stack-2x"></i>
+								<i class="fa fa-beer fa-stack-1x fa-inverse"></i>
+							</span>
+								<div class="panel panel-default text-left">
+									<div class="panel-body">
+										The night is dark and full of terrors. Bastards are born of passion, aren't they? We don't despise them in Dorne. Forgive my manners. I don't see many ladies these days. Lucky for the ladies. The War of the 5 kings.
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- sfooter content -->
+				<footer class="bg-info text-center">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4 col-md-offset-4">
+								Footer Sample for Bootstrap
+							</div>
+						</div>
+					</div>
+				</footer>
+	</body>
+</html>

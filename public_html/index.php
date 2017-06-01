81113031cec46980b29b355577299955d21f0d41
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-01 13:22:47 -0600

    add mail-config update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
+<html lang="en">
 	<!--
 	PWP for JCIT.INFO
 	@author Jeffrey Cooper <jcooper37@cnm.edu>
@@ -10,10 +10,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -23,26 +25,31 @@
 		<![endif]-->
 
 		<!-- Custome CSS -->
-		<link rel="stylesheet" href="public_html/css/stylesheet.css" type="text/css">
+		<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
 
 		<!-- Add FontAwesome -->
 		<script src="https://use.fontawesome.com/1a5ce32ebc.js"></script>
 
-<!--		TODO: Add family for Google Fonts-->
-<!--		Add Google Fonts-->
+		<!--		TODO: Add family for Google Fonts-->
+		<!--		Add Google Fonts-->
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=???">
 
 		<!-- jQuery 2.2.2 -->
-		<script src=https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.1/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.1/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
 
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -59,7 +66,8 @@
 					<div class="container-fluid">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
@@ -83,7 +91,7 @@
 			</header>
 
 			<main>
-<!--				Home Page-->
+				<!--				Home Page-->
 				<section>
 					<div class="container">
 						<div class="row">
@@ -133,7 +141,11 @@
 						</div>
 						<div class="row">
 							<div text-center>
-								<p>It's ten thousand miles between Kings landing and the wall. The bear and the maiden fair. You know nothing, Jon Snow. A Lannister always pays his debts. A good act does not wash out the bad, nor a bad act the good. Each should have its own reward. When you play the game of thrones, you win or you die. You know nothing, Jon Snow. It's ten thousand miles between Kings landing and the wall. Winter is coming.</p>
+								<p>It's ten thousand miles between Kings landing and the wall. The bear and the maiden fair. You
+									know nothing, Jon Snow. A Lannister always pays his debts. A good act does not wash out the
+									bad, nor a bad act the good. Each should have its own reward. When you play the game of
+									thrones, you win or you die. You know nothing, Jon Snow. It's ten thousand miles between
+									Kings landing and the wall. Winter is coming.</p>
 							</div>
 						</div>
 						<div class="row">
@@ -141,13 +153,17 @@
 								<a href="http://placehold.it" class="thumbnail">
 									<img src="http://placehold.it/171x120" alt="Website A">
 								</a>
-								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to come.</p>
+								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms
+									of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to
+									come.</p>
 							</div>
 							<div class="col-xs-12 col-md-6">
 								<a href="http://placehold.it" class="thumbnail">
 									<img src="http://placehold.it/171x120" alt="Website B">
 								</a>
-								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to come.</p>
+								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms
+									of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to
+									come.</p>
 							</div>
 						</div>
 						<div class="row">
@@ -155,13 +171,17 @@
 								<a href="http://placehold.it" class="thumbnail">
 									<img src="http://placehold.it/171x120" alt="Website A">
 								</a>
-								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to come.</p>
+								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms
+									of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to
+									come.</p>
 							</div>
 							<div class="col-xs-12 col-md-6">
 								<a href="http://placehold.it" class="thumbnail">
 									<img src="http://placehold.it/171x120" alt="Website B">
 								</a>
-								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to come.</p>
+								<p>A Lannister always pays his debts. Winter is coming. I am the shield that guards the realms
+									of men. I pledge my life and honor to the Night's Watch, for this night and all the nights to
+									come.</p>
 							</div>
 						</div>
 					</div>
@@ -173,22 +193,26 @@
 							<!-- nav-experience bar -->
 							<!-- Nav pills -->
 							<ul class="nav nav-pills" role="tablist">
-								<li role="presentation" class="active"><a href="#education" aria-controls="education" role="tab" data-toggle="pill">Experience</a></li>
-								<li role="presentation"><a href="#skills" aria-controls="skills" role="tab" data-toggle="pill">Skills</a></li>
-								<li role="presentation"><a href="#experience" aria-controls="experience" role="tab" data-toggle="pill">Experience</a></li>
-								<li role="presentation"><a href="#interests" aria-controls="interests" role="tab" data-toggle="pill">Interests</a></li>
+								<li role="presentation" class="active"><a href="#education" aria-controls="education" role="tab"
+																						data-toggle="pill">Experience</a></li>
+								<li role="presentation"><a href="#skills" aria-controls="skills" role="tab" data-toggle="pill">Skills</a>
+								</li>
+								<li role="presentation"><a href="#experience" aria-controls="experience" role="tab"
+																	data-toggle="pill">Experience</a></li>
+								<li role="presentation"><a href="#interests" aria-controls="interests" role="tab"
+																	data-toggle="pill">Interests</a></li>
 							</ul>
-								<!-- Tab panes -->
-								<div class="tab-content">
-									<div role="tabpanel" class="tab-pane fade in active" id="education">...</div>
-									<div role="tabpanel" class="tab-pane fade" id="skills">...</div>
-									<div role="tabpanel" class="tab-pane fade" id="experience">...</div>
-									<div role="tabpanel" class="tab-pane fade" id="interests">...</div>
-								</div>
-
+							<!-- Tab panes -->
+							<div class="tab-content">
+								<div role="tabpanel" class="tab-pane fade in active" id="education">...</div>
+								<div role="tabpanel" class="tab-pane fade" id="skills">...</div>
+								<div role="tabpanel" class="tab-pane fade" id="experience">...</div>
+								<div role="tabpanel" class="tab-pane fade" id="interests">...</div>
 							</div>
 
 						</div>
+
+					</div>
 				</section>
 				<!-- Contact Form -->
 				<section>
@@ -226,7 +250,8 @@
 								<div class="input-group-addon">
 									<i class="fa fa-comment" aria-hidden="true"></i>
 								</div>
-								<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message"></textarea>
+								<textarea class="form-control" rows="5" id="message" name="message"
+											 placeholder="Message"></textarea>
 							</div>
 						</div>
 						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
